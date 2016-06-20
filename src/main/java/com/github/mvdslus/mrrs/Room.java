7521028c9a5596b42e46426cbfe5fdac94731339
sluske <sluskes@kpnmail.nl>
2016-06-20 22:55:06 +0200

    Adds unit tests for room

--- src/main/java/com/github/mvdslus/mrrs/Room.java
+++ src/main/java/com/github/mvdslus/mrrs/Room.java
@@ -1,15 +1,38 @@
 package com.github.mvdslus.mrrs;
 
 public class Room {
-	String RoomName;
-	String RoomLocation;
-	int RoomCapacity;
+	String name;
+	String location;
+	int capacity;
 	
 	public Room(String name, String loc, int cap){
-		RoomName=name;
-		this.RoomCapacity = cap;
-		this.RoomLocation = loc;
+		this.name=name;
+		this.capacity = cap;
+		this.location = loc;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String roomName) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(String roomLocation) {
+		this.location = roomLocation;
+	}
+
+	public int getCapacity() {
+		return this.capacity;
+	}
+
+	public void setCapacity(int roomCapacity) {
+		this.capacity = roomCapacity;
+	}
 	
 }
--- src/test/java/com/github/mvdslus/mrrs/domain/RoomTest.java
+++ src/test/java/com/github/mvdslus/mrrs/domain/RoomTest.java
@@ -11,13 +11,26 @@ public class RoomTest {
     @Test
     public void createBasicRoom() throws Exception {
         String name = "Berlin";
-        String location = "01.02";
+        String location = "03.02";
         int capacity = 12;
         Room room = new Room(name, location, capacity);
         
-       // assertEquals(room.getName(), name);
-       // assertEquals(room.getLocation(), location);
-        // assertEquals(room.getCapacity(), capacity);
+       assertEquals(room.getName(), name);
+       assertEquals(room.getLocation(), location);
+       assertEquals(room.getCapacity(), capacity);
     }
 
+    
+    @Test
+    /*
+     * It should ot be possible to create a room with e negative capacity
+     */
+    public void createRoomWithNegativeCapicity() throws Exception{
+    	String name= " Negative";
+    	String location = " 99.99";
+    	int capacity = -3; 
+    	Room room = new Room(name, location, capacity);
+    	assertEquals(room.getCapacity(), capacity);
+    	
+    }
 }
